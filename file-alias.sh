80f13ff44fa71793c7201a6a3078b2f7d256860a
AbhinavAchha <achhaabhinav@gmail.com>
2022-09-07 11:44:17 +0530

    rm some aliases

--- file-alias.sh
+++ file-alias.sh
@@ -5,8 +5,6 @@ alias me='cd /media/extended/'
 alias mp='cd ~/MuPhile-Labs/MuERP/'
 alias rmp='cd ~/MuPhile-Labs/ReactMuERP/'
 alias scripts='cd $HOME/scripts'
-alias ulb='cd /usr/local/bin'
-alias cdlua='cd $HOME/scripts/lua'
 
 alias ..='cd ..'
 alias ...='cd ../..'
@@ -15,5 +13,4 @@ alias ,,='..'
 
 # File Aliases
 alias aliases='vim $HOME/aliases/aliases'
-alias vimrc='vim $HOME/neovim-config/init.lua'
 alias zshrc='vim $HOME/zsh/.zshrc'
